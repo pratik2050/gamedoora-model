e328111bbb006c43522a29e974466fe54ab9037e
Arko <47105136+arkaprovob@users.noreply.github.com>
2023-07-01 21:27:56 +0530

    This commit addresses an issue where I mistakenly used my personal namespace instead of the group namespace in one of the files.

--- check-version.sh
+++ check-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
-URL=https://maven.pkg.github.com/arkaprovob/gamedoora-model/com/gamedoora/gamedoora-model/$VERSION/gamedoora-model-$VERSION.pom
+URL=https://maven.pkg.github.com/gamedoora/gamedoora-model/com/gamedoora/gamedoora-model/$VERSION/gamedoora-model-$VERSION.pom
 STATUS_CODE=$(curl --silent --output /dev/null --write-out "%{http_code}\n" -u ${USER}:${PAT} $URL)
 
 if [ $STATUS_CODE -eq 404 ]; then
