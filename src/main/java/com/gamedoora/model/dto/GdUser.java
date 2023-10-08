208401415b8fd50512b337cc4d518409a5171158
picksitquick <picksitquick@gmail.com>
2023-10-08 19:28:18 +0530

    updated GdUser to add Studios

--- src/main/java/com/gamedoora/model/dto/GdUser.java
+++ src/main/java/com/gamedoora/model/dto/GdUser.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 @EqualsAndHashCode(callSuper = false)
 @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GdUser {
     private List<RoleDTO> roles;
     private List<SkillsDTO> skills;
